e4a90ed1ede6e6a82be35ff51dea83174b890b3f
Andrey Vlasenko <andrewvenko95@gmail.com>
2023-09-10 00:54:44 +0300

    Layot footer mobile

--- src/TS/script.ts
+++ src/TS/script.ts
@@ -1,5 +1,5 @@
 const textBtn = setInterval(() => {
-    if (window.innerWidth < 641) {
+    if (window.innerWidth <0 640) {
         document.querySelector('.delivery__btn').textContent = 'Узнать подробнее';
     } else {
         document.querySelector('.delivery__btn').textContent = 'Расчитать доставку';
