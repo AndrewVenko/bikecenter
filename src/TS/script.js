00c17c7eedce66da356d999cc5aea54c5120399c
Andrey Vlasenko <andrewvenko95@gmail.com>
2023-09-16 18:32:41 +0300

    Layot mobile footer

--- src/TS/script.js
+++ src/TS/script.js
@@ -1,9 +1,16 @@
 "use strict";
 const textBtn = setInterval(() => {
-    if (window.innerWidth < 641) {
+    if (window.innerWidth <= 640) {
         document.querySelector('.delivery__btn').textContent = 'Узнать подробнее';
+        document.querySelector('.info-contacts__mobile-text-watsapp').textContent = 'Написать в WhatsApp';
+        document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Написать в Telegram';
+        document.querySelector('.info__link-mobile').textContent = 'Позвонить 8 (800) 500-03-20';
     }
     else {
         document.querySelector('.delivery__btn').textContent = 'Расчитать доставку';
+        document.querySelector('.info-contacts__mobile-text-watsapp').textContent = 'WhatsApp';
+        document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Telegram';
+        document.querySelector('.info__link-mobile').textContent = '8 (800) 500-03-20';
     }
+    ;
 }, 1);
--- src/TS/script.ts
+++ src/TS/script.ts
@@ -1,7 +1,19 @@
 const textBtn = setInterval(() => {
-    if (window.innerWidth <0 640) {
+    if (window.innerWidth <= 640) {
         document.querySelector('.delivery__btn').textContent = 'Узнать подробнее';
+
+        document.querySelector('.info-contacts__mobile-text-watsapp').textContent = 'Написать в WhatsApp';
+
+        document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Написать в Telegram';
+
+        document.querySelector('.info__link-mobile').textContent = 'Позвонить 8 (800) 500-03-20';
     } else {
         document.querySelector('.delivery__btn').textContent = 'Расчитать доставку';
-    }
+
+        document.querySelector('.info-contacts__mobile-text-watsapp').textContent = 'WhatsApp';
+
+        document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Telegram';
+
+        document.querySelector('.info__link-mobile').textContent = '8 (800) 500-03-20';
+    };
 }, 1);
