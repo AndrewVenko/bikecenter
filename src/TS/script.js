23944cece29a28fa9c5b824c5e06f7edae20f85e
Andrey Vlasenko <andrewvenko95@gmail.com>
2023-09-19 20:11:17 +0300

    Layot mobile footer

--- src/TS/script.js
+++ src/TS/script.js
@@ -1,16 +1,25 @@
 "use strict";
 const textBtn = setInterval(() => {
+    var _a;
     if (window.innerWidth <= 640) {
         document.querySelector('.delivery__btn').textContent = 'Узнать подробнее';
         document.querySelector('.info-contacts__mobile-text-watsapp').textContent = 'Написать в WhatsApp';
         document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Написать в Telegram';
         document.querySelector('.info__link-mobile').textContent = 'Позвонить 8 (800) 500-03-20';
+        document.querySelector('.info-form__header').textContent = 'Подпишитесь на наши новости';
+        document.querySelector('.info-form__input').placeholder = 'Введите свой e-mail';
+        document.querySelector('.info-form__btn').value = '\u{1F862}';
+        document.querySelector('.copyrights__text-mobile').textContent = `Интернет-магазин "Байк Центр" ® bike-сentre.ru`;
     }
     else {
         document.querySelector('.delivery__btn').textContent = 'Расчитать доставку';
         document.querySelector('.info-contacts__mobile-text-watsapp').textContent = 'WhatsApp';
         document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Telegram';
         document.querySelector('.info__link-mobile').textContent = '8 (800) 500-03-20';
+        (_a = document.querySelector('.info-form__header')) === null || _a === void 0 ? void 0 : _a.textContent = 'Узнайте о скидках первым';
+        document.querySelector('.info-form__input').placeholder = 'Ваш email-адрес';
+        document.querySelector('.info-form__btn').value = 'Подписаться';
+        document.querySelector('.copyrights__text-mobile').textContent = `Сеть магазинов "Байк Центр" ® — bike-сentre.ru`;
     }
     ;
 }, 1);
--- src/TS/script.ts
+++ src/TS/script.ts
@@ -7,6 +7,15 @@ const textBtn = setInterval(() => {
         document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Написать в Telegram';
 
         document.querySelector('.info__link-mobile').textContent = 'Позвонить 8 (800) 500-03-20';
+
+        document.querySelector('.info-form__header').textContent = 'Подпишитесь на наши новости';
+
+        document.querySelector('.info-form__input').placeholder = 'Введите свой e-mail';
+
+        document.querySelector('.info-form__btn').value = '\u{1F862}';
+
+        document.querySelector('.copyrights__text-mobile').textContent = `Интернет-магазин "Байк Центр" ® bike-сentre.ru`;
+        
     } else {
         document.querySelector('.delivery__btn').textContent = 'Расчитать доставку';
 
@@ -15,5 +24,13 @@ const textBtn = setInterval(() => {
         document.querySelector('.info-contacts__mobile-text-telegram').textContent = 'Telegram';
 
         document.querySelector('.info__link-mobile').textContent = '8 (800) 500-03-20';
+
+        document.querySelector('.info-form__header')?.textContent = 'Узнайте о скидках первым';
+
+        document.querySelector('.info-form__input').placeholder = 'Ваш email-адрес';
+
+        document.querySelector('.info-form__btn').value = 'Подписаться';
+
+        document.querySelector('.copyrights__text-mobile').textContent = `Сеть магазинов "Байк Центр" ® — bike-сentre.ru`;
     };
 }, 1);
